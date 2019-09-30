1063661d32805e0d03165d1678fdcd7dbf3d5505
unknown <E:\Tools' data\The Bat!>
2019-09-30 16:16:01 +0500

    browser.proxy.onError firefox fix

--- code/api/proxy.js
+++ code/api/proxy.js
@@ -7,8 +7,19 @@ const ns = require( '../ns' );
 
 
 module.exports = () => {
-  if( typeof ns.proxy !== 'object' || promiseSupport ) return ns.proxy;
+  if( typeof ns.proxy !== 'object' ) return ns.proxy;
+
+  // FF
+  if( promiseSupport ) {
+    return bindAll({
+      'onError': ns.proxy.onError || ns.proxy.onProxyError
+    }, ns.proxy, {
+      'methods': [ 'register', 'registerProxyScript', 'unregister' ],
+      'objects': [ 'onProxyError', 'onRequest', 'settings' ]
+    });
+  }
   
+  // Chrome
   return bindAll({
     'onError': ns.proxy.onError || ns.proxy.onProxyError
   }, ns.proxy, {
